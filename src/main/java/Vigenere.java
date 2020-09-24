1f7921f17c144762319315d7d00abeb4191ba619
2022jobyrne <22jobyrne@athenian.org>
2020-09-24 07:05:49 +0000

    not completed - need help with debugging

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -2,14 +2,89 @@ import java.util.Scanner;
 
 public class Vigenere {
     public static String encryptVigenere(String message, String key) {
+        String realmessage = message.toUpperCase();
+        String realkey = key.toUpperCase();
+        int i = 0;
+        int i2 = 0;
+        int shift = 0;
+        //int shift2 = 0;
+        int other = 0;
+        char val = ' ';
+        char c1 = ' ';
+        char c2 = ' ';
+        String str2 = "";
+        //char value = ' ';
+        int length = realmessage.length();
+        int klength = realkey.length();
+        while (i <= length) {   // && i <= length) { //finding shift
+            if (i2 == klength) {
+            i = 0;
+        }
+            val = realkey.charAt(i); //initial char
+            //hypoethetical c
+
+            other = val - 'A'; //3
+            shift = other; //3 need to have it repeat the sequence, not just all three
+            // c = 54
+            //a = 52
+            // a - c (chars) = the amount of the shift for each letter
+            //shift2 = (char) val - 65;
+            //int number = shift2;
+
+            //for (i ) while (i <= length) {
+            
+                c1 = realmessage.charAt(i);
+            if ((c1 >= 91 && c1 <= 96) || (c1 >= 123 || c1 <= 40)) {
+                c2 = c1;
+            }
+            else {
+                c2 = (char) (c1 + shift);
+            // add code for finding the specific location of each character(eg c = 0 and a = 1) about shift = 
+            }
+        str2 = str2 + c2;
+        i++;
+        i2++;
         
-        return message;
-        // REPLACE THIS WITH YOUR CODE
     }
+     return str2;
+}
+
+    
 
     public static String decryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String realmessage = message.toUpperCase();
+        String realkey = key.toUpperCase();
+        int i = 0;
+        int i2 = 0;
+        int shift = 0;
+        int other = 0;
+        char val = ' ';
+        char c1 = ' ';
+        char c2 = ' ';
+        String str2 = "";
+        int length = realmessage.length();
+        int klength = realkey.length();
+        while (i <= length) {
+            if (i2 == klength) {
+            i = 0;
+        }
+            val = realkey.charAt(i); 
+            other = val - 'A'; 
+            shift = other;
+            c1 = realmessage.charAt(i);
+            if ((c1 >= 91 && c1 <= 96) || (c1 >= 123 || c1 <= 40)) {
+                c2 = c1;
+            }
+            else {
+                c2 = (char) (c1 - shift);
+            
+            }
+        str2 = str2 + c2;
+        i++;
+        i2++;
+        
+    }
+     return str2;
     }
 
 
